26c2d3ca1a05518f4479dcc44b041b37877e1575
idkwhoim <70272254+idkwhoim@users.noreply.github.com>
2020-08-30 17:14:19 +0800

    Update mss-centos.sh

--- mss-centos.sh
+++ mss-centos.sh
@@ -11,6 +11,7 @@ FREEMEM=`free -m | grep Mem: | awk '{print $7}'`
 SMIN=`expr $FREEMEM / 2`
 SMAX=`expr $FREEMEM - 200`
 DIRECTORY=`pwd`
+SERVERPID=`pidof java`
 read -p "请输入您的选项(1-3):" CHOICE1
 if [ $USER=root ]
     then case $CHOICE1 in
@@ -24,26 +25,28 @@ if [ $USER=root ]
              read -p "请输入最大内存(默认建议内存为$SMAX):" MAXMEM
              [ -z "$MAXMEM" ] && MAXMEM=$SMAX
              echo "正在下载服务端，请不要操作"
-             wget -P $DIRECTORY https://s3.amazonaws.com/Minecraft.Download/versions/$GAMEVERSION/minecraft_server.$GAMEVERSION.jar -q
-             touch $DIRECTORY\eula.txt
-             echo "eula=true" >> $DIRECTORY\eula.txt
-             nohup java -Xms$MINMEM\m -Xmx$MAXMEM\m -jar $DIRECTORY/minecraft_server.$GAMEVERSION.jar nogui &>> server.log &
-             if [ $? -eq 0 ]
-                 then echo "服务器开启成功，你可以关闭窗口了"
-                 else echo "服务器开启失败!"
+             wget -P $DIRECTORY https://raw.githubusercontent.com/idkwhoim/MinecraftServerArchieve/master/Jar/minecraft_server.$GAMEVERSION.jar -q
+             if [ ! -e $DIRECTORY/minecraft_server.$GAMEVERSION.jar ]
+                 then echo "下载失败"
+                      exit
              fi
+             touch $DIRECTORY/eula.txt
+             echo "eula=true" >> $DIRECTORY/eula.txt
+             echo "正在开启服务器"
+             nohup java -Xms$MINMEM\m -Xmx$MAXMEM\m -jar $DIRECTORY/minecraft_server.$GAMEVERSION.jar nogui &>> server.log &
+             [ -z "$SERVERPID" ] && echo "服务器开启失败!" || echo "服务器开启成功!"
              ;;
          2) 
-             SERVERPID=`pidof java`
              if [ -z "$SERVERPID" ]
                  then read -p "服务器未开启，输入1开启:" CHOICE2
                       if [ $CHOICE2 -eq 1 ]
-                          then read -p "请输入服务器创建目录(默认本目录)" DIRECTORY                     
+                          then read -p "请输入服务器创建目录(默认本目录)" DIRECTORY
+                               [ -z "$DIRECTORY" ] && DIRECTORY=`pwd`
                                read -p "请输入最小内存(默认建议内存为$SMIN):" MINMEM
                                [ -z "$MINMEM" ] && MINMEM=$SMIN
                                read -p "请输入最大内存(默认建议内存为$SMAX):" MAXMEM
                                [ -z "$MAXMEM" ] && MAXMEM=$SMAX
-                               ALLVERSION=`find -name "$DIRECTORY/minecraft_server.*.jar" | wc -l`
+                               ALLVERSION=`find $DIRECTORY -name "minecraft_server.*.jar" | wc -l`
                                if [ $ALLVERSION -eq 1 ]
                                    then nohup java -Xms$MINMEM\m -Xmx$MAXMEM\m -jar $DIRECTORY/minecraft_server.*.jar nogui &>> server.log &
                                         if [ $? -eq 0 ]
@@ -54,14 +57,14 @@ if [ $USER=root ]
                                            then echo "目录下未找到服务端"
                                            else echo "有多个服务端"
                                         fi
-                               fi       
-                          else read -p "服务器正在运行 输入1关闭服务器:" CHOICE3
-                               if [ $CHOICE3 -eq 1 ]
-                                   then kill $SERVERPID
-                                        echo "关闭成功"
-                                   else exit
                                fi
-                      fi
+                       fi        
+                  else read -p "服务器正在运行 输入1关闭服务器:" CHOICE3
+                       if [ $CHOICE3 -eq 1 ]
+                           then kill $SERVERPID
+                           echo "关闭成功"
+                           else exit
+                       fi
              fi
              ;;
          3)
